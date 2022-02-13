62b48d8cbdc5d96fb8d8820dc701489d3a32271f
JT5519 <joel.sk10@gmail.com>
2022-02-13 18:18:55 -0500

    Event Dispatching works!

--- Engine/src/Engine/Application.cpp
+++ Engine/src/Engine/Application.cpp
@@ -1,20 +1,29 @@
 #include "hzpch.h"
 #include "Application.h"
 
-#include "Engine/Events/ApplicationEvent.h"
 #include "Engine/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Engine {
-
+	
+	#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		HZ_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -27,4 +36,9 @@ namespace Engine {
 		}
 	}
 
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
 }
