5bd005e852953bf9689caf08bffc3ea438b18a93
JT5519 <joel.sk10@gmail.com>
2022-02-13 15:45:48 -0500

    Window Implementation completed

--- Engine/src/Engine/Application.cpp
+++ Engine/src/Engine/Application.cpp
@@ -10,6 +10,7 @@ namespace Engine
 {
 	Application::Application()
 	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 
 	Application::~Application()
@@ -26,6 +27,11 @@ namespace Engine
 		{
 			HZ_TRACE(e);
 		}
-		while (true);
+		while (m_Running)
+		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
 	}
 }
