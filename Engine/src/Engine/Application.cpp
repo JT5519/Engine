a141e4e3e8d2492f9ae151e383694de32c9e815d
JT5519 <joel.sk10@gmail.com>
2022-02-20 14:18:03 -0500

    Fixed multi-layer debug issues

--- Engine/src/Engine/Application.cpp
+++ Engine/src/Engine/Application.cpp
@@ -4,12 +4,36 @@
 #include "Engine/Log.h"
 
 #include <glad/glad.h>
+
 #include "Input.h"
 
 namespace Engine {
-	
-	#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application* Application::s_Instance = nullptr;
+
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case Engine::ShaderDataType::Float:    return GL_FLOAT;
+			case Engine::ShaderDataType::Float2:   return GL_FLOAT;
+			case Engine::ShaderDataType::Float3:   return GL_FLOAT;
+			case Engine::ShaderDataType::Float4:   return GL_FLOAT;
+			case Engine::ShaderDataType::Mat3:     return GL_FLOAT;
+			case Engine::ShaderDataType::Mat4:     return GL_FLOAT;
+			case Engine::ShaderDataType::Int:      return GL_INT;
+			case Engine::ShaderDataType::Int2:     return GL_INT;
+			case Engine::ShaderDataType::Int3:     return GL_INT;
+			case Engine::ShaderDataType::Int4:     return GL_INT;
+			case Engine::ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		HZ_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		HZ_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -24,16 +48,36 @@ namespace Engine {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			 0.5f, -0.5f, 0.0f,
-			 0.0f,  0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		{
+			BufferLayout layout = {
+				{ ShaderDataType::Float3, "a_Position" },
+				{ ShaderDataType::Float4, "a_Color" }
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(const void*)element.Offset);
+			index++;
+		}
 
 		uint32_t indices[3] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -42,10 +86,13 @@ namespace Engine {
 			#version 330 core
 			
 			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
 			out vec3 v_Position;
+			out vec4 v_Color;
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);	
 			}
 		)";
@@ -55,9 +102,11 @@ namespace Engine {
 			
 			layout(location = 0) out vec4 color;
 			in vec3 v_Position;
+			in vec4 v_Color;
 			void main()
 			{
 				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
@@ -67,6 +116,7 @@ namespace Engine {
 	Application::~Application()
 	{
 	}
+
 	void Application::PushLayer(Layer* layer)
 	{
 		m_LayerStack.PushLayer(layer);
@@ -76,18 +126,18 @@ namespace Engine {
 	{
 		m_LayerStack.PushOverlay(layer);
 	}
+
 	void Application::OnEvent(Event& e)
 	{
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
-		
+
 		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
 		{
 			(*--it)->OnEvent(e);
 			if (e.Handled)
 				break;
 		}
-		
 	}
 
 	void Application::Run()
@@ -103,7 +153,7 @@ namespace Engine {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
-			
+
 			m_ImGuiLayer->Begin();
 			for (Layer* layer : m_LayerStack)
 				layer->OnImGuiRender();
@@ -118,4 +168,5 @@ namespace Engine {
 		m_Running = false;
 		return true;
 	}
+
 }
